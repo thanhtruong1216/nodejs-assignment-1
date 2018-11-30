3d0b2495c83d6ff389472921351d3f25692fabf3
Thanh Thanh <thanhtruong1216@gmail.com>
2018-11-30 20:01:20 +0700

    Refactor: Use arrow function

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ let fs = require('fs');
 
 // Instantiate the HTTP server
 let httpServer = http.createServer((req, res) => {
-  unifiedServer(req, res)
+  unifiedServer(req, res);
 });
 
 // Start the HTTP server and have it listen on port 3000
 httpServer.listen(config.httpPort, () => {
-  console.log('The server is listening on port ' + config.httpPort)
+  console.log('The server is listening on port ' + config.httpPort);
 });
 
 // Instantiate the HTTPS server
@@ -21,8 +21,9 @@ let httpsServerOptions = {
   'key': fs.readFileSync('./https/key.pem', 'utf8'),
   'cert': fs.readFileSync('./https/cert.pem', 'utf8')
 }
+
 let httpsServer = https.createServer(httpsServerOptions, (req, res) => {
-  unifiedServer(req, res)
+  unifiedServer(req, res);
 });
 
 // Start the HTTPS server
@@ -46,34 +47,38 @@ let unifiedServer = (req, res) => {
   let method = req.method.toLowerCase();
 
 // Get the header as an object
-  var header = req.headers;
+  let header = req.headers;
 
 // Get the payload, if any
-var decoder = new stringDecoder('utf-8');
-var buffer = '';
+let decoder = new stringDecoder('utf-8');
+let buffer = '';
 req.on('data', data => {
   buffer += decoder.write(data);
 });
-req.on('end', function() {
-  buffer += decoder.end()
+req.on('end', () => {
+  buffer += decoder.end();
   // Choose the handler this request should go to. If one is not found, use the not found handler
-  var chosenHandler = typeof(router[trimmePath]) !== 'undefined' ? router[trimmePath] : handlers.notFound;
+  let chosenHandler = typeof(router[trimmePath]) !== 'undefined' ? router[trimmePath] : handlers.notFound;
+
   // Construct the data object to send to the handler, or default to an empty object
-  var data = {
+  let data = {
     'trimmePath': trimmePath,
     'queryStringObject': queryStringObject,
     'method': method,
     'headers': header,
     'payload': buffer,
   }
+
   // Route the request to the handler specified in the router
   chosenHandler(data, (statusCode, payload) => {
     // Use the status code called back by the handler, or defalut to 200
     statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
     // Use the payload call back by the handler
     payload = typeof(payload) == 'object' ? payload : {};
+
     // Convert the payload to a string
-    var payloadString = JSON.stringify(payload);
+    let payloadString = JSON.stringify(payload);
 
     // Return the response
     res.setHeader('Content-Type', 'application/json');
@@ -87,19 +92,19 @@ req.on('end', function() {
 }
 
 // Define the handlers
-var handlers = {};
+let handlers = {};
 
 // Route handler
 handlers.route = (data, callback) => {
-  callback(200, { message: 'Thank for your help' })
+  callback(200, { message: 'No NPM!' });
 };
 
 // Handler not found
 handlers.notFound = (data, callback) => {
-  callback(404)
+  callback(404);
 };
 
 // Define a request router
-var router = {
+let router = {
   'hello': handlers.route
 }
